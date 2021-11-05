0c9520f17313002460847b5139814c1b2c35db70
Philip Crotwell <crotwell@seis.sc.edu>
2021-11-05 18:40:34 -0400

    fixes per test-bot

--- Formula/seisfile.rb
+++ Formula/seisfile.rb
@@ -1,5 +1,5 @@
 class Seisfile < Formula
-  desc "library for reading and writing seismic file formats in java"
+  desc "Library for reading and writing seismic file formats in java"
   homepage "https://www.seis.sc.edu/seisFile.html"
   url "https://github.com/crotwell/seisFile/releases/download/v2.0.2/seisFile-2.0.2.tgz"
   sha256 "03709a0eee72ff8a5c94f73d6f854efebb79c3fc5571df30280c830c120e6ae3"
@@ -8,7 +8,7 @@ class Seisfile < Formula
   def install
     rm_f Dir["bin/*.bat"]
     man1.install "docs/manpage/seisfile.1"
-    etc.install Dir["docs/bash_completion.d"]
+    etc.install "docs/bash_completion.d"
     libexec.install %w[bin lib]
     env = if Hardware::CPU.arm?
       Language::Java.overridable_java_home_env("11")
