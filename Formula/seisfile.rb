1cc15e5b04987cda18470b2e8c5f30bfd302ae8e
Philip Crotwell <crotwell@seis.sc.edu>
2025-05-15 10:01:04 -0400

    2.3.0 and brew style fix

--- Formula/seisfile.rb
+++ Formula/seisfile.rb
@@ -1,12 +1,12 @@
 class Seisfile < Formula
   desc "Library for reading and writing seismic file formats in java"
   homepage "https://www.seis.sc.edu/seisFile.html"
-  url "https://github.com/crotwell/seisFile/releases/download/v2.0.3/seisFile-2.0.3.tgz"
-  sha256 "43d2229adeb157a809a394fc3dc14f14c953c8ce48d245d46f8f9cafa1b0e1f2"
+  url "https://github.com/crotwell/seisFile/releases/download/v2.3.0/seisFile-2.3.0.tar"
+  sha256 "64eb776679a18e186135d6f35e9efd86e538ae5a0300a3bb30224a862d9067ea"
   license "LGPL-3.0-or-later"
 
   def install
-    rm_f Dir["bin/*.bat"]
+    rm Dir["bin/*.bat"]
     man1.install "docs/manpage/seisfile.1"
     etc.install "docs/bash_completion.d"
     libexec.install %w[bin lib]
