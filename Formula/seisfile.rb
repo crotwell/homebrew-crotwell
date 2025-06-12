5cdceb6fcef780cfb9b8e855f1f834859bce4daa
Philip Crotwell <crotwell@seis.sc.edu>
2025-06-12 10:46:27 -0400

    bad comment for ruby

--- Formula/seisfile.rb
+++ Formula/seisfile.rb
@@ -4,13 +4,13 @@ class Seisfile < Formula
   url "https://github.com/crotwell/seisFile/releases/download/v2.3.0/seisFile-2.3.0.tar"
   sha256 "64eb776679a18e186135d6f35e9efd86e538ae5a0300a3bb30224a862d9067ea"
   license "LGPL-3.0-or-later"
-  revision 1
+  revision 2
   depends_on "openjdk"
 
   def install
     rm Dir["bin/*.bat"]
     man1.install "docs/manpage/seisfile.1"
-    #etc.install "docs/bash_completion.d"
+    # etc.install "docs/bash_completion.d"
     libexec.install %w[bin lib]
     (bin/"seisfile").write_env_script libexec/"bin/seisfile", env
   end
